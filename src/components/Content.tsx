27d5c60de6df954b717d50b1af6efd19dd020c5c
Diego Alves Santana <diegoalsantana@gmail.com>
2021-05-17 21:42:15 -0300

    Finalizacao de desafio

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,24 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+export function Content(props: any) {
+  return (
+    <header>
+      <span className="category">
+        Categoria:<span> {props.selectedGenre}</span>
+      </span>
+      <main>
+        <div className="movies-list">
+          {props.movies.map((movie: any) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </header>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,22 @@
-export function SideBar() {
-  // Complete aqui
+import { useState } from "react";
+import { Button } from "./Button";
+
+export function SideBar(props: any) {
+  const { onClick } = props;
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  return props.genres.map((genre: any) => (
+    <Button
+      key={String(genre.id)}
+      title={genre.title}
+      iconName={genre.name}
+      onClick={() => {
+        handleClickButton(genre.id);
+      }}
+      selected={selectedGenreId === genre.id}
+    />
+  ));
+  function handleClickButton(id: any) {
+    onClick(id);
+    setSelectedGenreId(id);
+  }
 }
